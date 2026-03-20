9e09d6c1feba43fbe92b4924098cfd3f456bea57
Nguyen Hung <17531153+HungNguyen10082003@users.noreply.github.com>
2026-03-20 17:13:22 +0700

    'bt'

--- Module3/bt/thaotacchtt.sql
+++ Module3/bt/thaotacchtt.sql
@@ -16,5 +16,5 @@ HAVING AVG(Mark) > 15;
 SELECT S.StudentId, S.StudentName, AVG(Mark)
 FROM Student S join Mark M on S.StudentId = M.StudentId
 GROUP BY S.StudentId, S.StudentName
-HAVING AVG(Mark) >= ALL (SELECT AVG(Mark) FROM Mark GROUP BY Mark.StudentId);
+HAVING AVG(Mark) >= ALL (SELECT AVG(Mark) FROM Mark GROUP BY Mark.StudentId);chi_tiet_don_hang
 
