9a7447a1d788041176a28949fc056237e1a400bb
Nguyen Hung <17531153+HungNguyen10082003@users.noreply.github.com>
2026-04-08 20:35:40 +0700

    mvc

---
+++ Module3/ss11_mvc/ss10/src/main/java/org/example/ss10/service/CustomerService.java
@@ -0,0 +1,17 @@
+package org.example.ss10.service;
+
+import org.example.ss10.model.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void update(int id, Customer customer);
+
+    void remove(int id);
+}
